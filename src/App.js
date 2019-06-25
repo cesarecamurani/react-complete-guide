843d391692ef0d2f731109337a7388f5ded8d96e
Cesare Camurani <cesare@bitpesa.co>
2019-06-25 16:58:45 +0100

    First commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import Person from './Person/Person';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi! I'm a React App</h1>
+        <Person />
+      </div>
+    );
+  }
+}
+
+export default App;
