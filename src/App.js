8c8a493d30239b93b65acf13173ac22b98c10bc9
Cesare Camurani <cesare@bitpesa.co>
2019-06-26 17:00:26 +0100

    Created Person component

--- src/App.js
+++ src/App.js
@@ -7,7 +7,9 @@ class App extends Component {
     return (
       <div className="App">
         <h1>Hi! I'm a React App</h1>
-        <Person />
+        <Person name="Cesare" age="36"/>
+        <Person name="Manu" age="26"/>
+        <Person name="Luca" age="30"/>
       </div>
     );
   }
