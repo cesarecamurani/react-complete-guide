5d6801de9906e9b46f90ab0816a442787b81d51c
Cesare Camurani <cesare@bitpesa.co>
2019-06-28 17:17:45 +0100

    App.js refactored with functional components approach

--- src/App.js
+++ src/App.js
@@ -1,20 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component, useState } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-class App extends Component {
-  state = {
+const app = props => {
+  const [ peopleState, setPeopleState ] = useState({
     people: [
       { name: 'Cesare', age: '36'},
       { name: 'Manu', age: '26'},
       { name: 'Luca', age: '30'}
     ]
-  }
+  })
 
-  switchNameHandler = () => {
-    // console.log('It was clicked!')
-    // this.state.people[0].name = 'Cesarone' --> Wrong way!!!
-    this.setState({
+  const switchNameHandler = () => {
+    setPeopleState({
       people: [
         { name: 'Cesarone', age: '36'},
         { name: 'Manu', age: '26'},
@@ -23,17 +21,42 @@ class App extends Component {
     )
   }
 
-  render() {
-    return (
-      <div className="App">
-        <h1>Hi! I'm a React App</h1>
-        <button onClick={this.switchNameHandler}> Switch Name </button>
-        <Person name={this.state.people[0].name} age={this.state.people[0].age}> Watching TV Series </Person>
-        <Person name={this.state.people[1].name} age={this.state.people[1].age}> Coding </Person>
-        <Person name={this.state.people[2].name} age={this.state.people[2].age}> Eating Vegan </Person>
-      </div>
-    );
-  }
+  return (
+    <div className="App">
+      <h1>Hi! I'm a React App</h1>
+      <button onClick={switchNameHandler}> Switch Name </button>
+      <Person
+        name={peopleState.people[0].name}
+        age={peopleState.people[0].age}> Watching TV Series </Person>
+      <Person
+        name={peopleState.people[1].name}
+        age={peopleState.people[1].age}> Coding </Person>
+      <Person
+        name={peopleState.people[2].name}
+        age={peopleState.people[2].age}> Eating Vegan </Person>
+    </div>
+  );
 }
 
-export default App;
+
+export default app;
+
+// state = {
+//   people: [
+//     { name: 'Cesare', age: '36'},
+//     { name: 'Manu', age: '26'},
+//     { name: 'Luca', age: '30'}
+//   ]
+// }
+//
+// switchNameHandler = () => {
+//   // console.log('It was clicked!')
+//   // this.state.people[0].name = 'Cesarone' --> Wrong way!!!
+//   this.setState({
+//     people: [
+//       { name: 'Cesarone', age: '36'},
+//       { name: 'Manu', age: '26'},
+//       { name: 'Luca', age: '30'}
+//     ]}
+//   )
+// }
