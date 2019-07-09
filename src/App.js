62ceafaa260cef0531d3c84c0ff06c6d6ae3abc1
Cesare Camurani <cesare@bitpesa.co>
2019-07-09 17:26:52 +0100

    Added switchNameHandler to Person.js

--- src/App.js
+++ src/App.js
@@ -1,62 +1,93 @@
-import React, { Component, useState } from 'react';
+import React, { Component } from 'react';
 import Person from './Person/Person';
 import './App.css';
 
-const app = props => {
-  const [ peopleState, setPeopleState ] = useState({
+class App extends Component {
+  state = {
     people: [
       { name: 'Cesare', age: '36'},
       { name: 'Manu', age: '26'},
       { name: 'Luca', age: '30'}
     ]
-  })
+  }
 
-  const switchNameHandler = () => {
-    setPeopleState({
+  switchNameHandler = (newName) => {
+    // console.log('It was clicked!')
+    // this.state.people[0].name = 'Cesarone' --> Wrong way!!!
+    this.setState({
       people: [
-        { name: 'Cesarone', age: '36'},
+        { name: newName, age: '36'},
         { name: 'Manu', age: '26'},
         { name: 'Luca', age: '30'}
       ]}
     )
   }
 
-  return (
-    <div className="App">
-      <h1>Hi! I'm a React App</h1>
-      <button onClick={switchNameHandler}> Switch Name </button>
-      <Person
-        name={peopleState.people[0].name}
-        age={peopleState.people[0].age}> Watching TV Series </Person>
-      <Person
-        name={peopleState.people[1].name}
-        age={peopleState.people[1].age}> Coding </Person>
-      <Person
-        name={peopleState.people[2].name}
-        age={peopleState.people[2].age}> Eating Vegan </Person>
-    </div>
-  );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hi! I'm a React App</h1>
+        <button onClick={() => this.switchNameHandler("Cesarone")}> Switch Name </button>
+        <Person
+          name={this.state.people[0].name}
+          age={this.state.people[0].age}> Watching TV Series </Person>
+        <Person
+          name={this.state.people[1].name}
+          age={this.state.people[1].age}
+          click={this.switchNameHandler.bind(this, "Ceasar")}> Coding </Person>
+        <Person
+          name={this.state.people[2].name}
+          age={this.state.people[2].age}> Eating Vegan </Person>
+      </div>
+    );
+  }
 }
 
+export default App;
 
-export default app;
 
-// state = {
-//   people: [
-//     { name: 'Cesare', age: '36'},
-//     { name: 'Manu', age: '26'},
-//     { name: 'Luca', age: '30'}
-//   ]
-// }
+// Functional approach below (with setState() hook example):
+
+// import React, { Component, useState } from 'react';
+// import Person from './Person/Person';
+// import './App.css';
 //
-// switchNameHandler = () => {
-//   // console.log('It was clicked!')
-//   // this.state.people[0].name = 'Cesarone' --> Wrong way!!!
-//   this.setState({
+// const app = props => {
+//   const [ peopleState, setPeopleState ] = useState({
 //     people: [
-//       { name: 'Cesarone', age: '36'},
+//       { name: 'Cesare', age: '36'},
 //       { name: 'Manu', age: '26'},
 //       { name: 'Luca', age: '30'}
-//     ]}
-//   )
+//     ]
+//   })
+//
+//   const switchNameHandler = () => {
+//     setPeopleState({
+//       people: [
+//         { name: 'Cesarone', age: '36'},
+//         { name: 'Manu', age: '26'},
+//         { name: 'Luca', age: '30'}
+//       ]}
+//     )
+//   }
+//
+//   return (
+//     <div className="App">
+//       <h1>Hi! I'm a React App</h1>
+//       <button onClick={switchNameHandler}> Switch Name </button>
+//       <Person
+//         name={peopleState.people[0].name}
+//         age={peopleState.people[0].age}> Watching TV Series </Person>
+//       <Person
+//         name={peopleState.people[1].name}
+//         age={peopleState.people[1].age}> Coding </Person>
+//       <Person
+//         name={peopleState.people[2].name}
+//         age={peopleState.people[2].age}> Eating Vegan </Person>
+//     </div>
+//   );
 // }
+//
+//
+// export default app;
+//
