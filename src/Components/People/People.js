bca0f479adf113680c9f042d6e39669210dc9192
Cesare Camurani <cesare@bitpesa.co>
2019-08-14 17:02:42 +0100

    Added lifecycle methods in People.js

--- src/Components/People/People.js
+++ src/Components/People/People.js
@@ -2,6 +2,33 @@ import React, { Component } from 'react'
 import Person from './Person/Person'
 
 class People extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      showPeople: false
+    }
+  }
+
+  static getDerivedStateFromProps(props, state) {
+    console.log('[People.js] getDerivedstatefromProps')
+    return state
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log('[People.js] shouldComponentUpdate')
+    return true
+  }
+
+  getSnapshotBeforeUpdate(prevProps, prevState) {
+    console.log('[People.js] getSnapshotBeforeUpdate')
+    return { message: 'Snapshot!'}
+  }
+
+  componentDidUpdate(prevProps, prevStat, snapshot) {
+    console.log('[People.js] componentDidUpdate')
+    console.log(snapshot)
+  }
+
   render() {
     console.log('[People.js] People')
     return this.props.people.map((person, index) => {
